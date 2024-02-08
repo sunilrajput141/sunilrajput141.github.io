0dec52be515019fd2d79d11a6c85c71cb3a99be1
sunilrajput141 <159447172+sunilrajput141@users.noreply.github.com>
2024-02-08 14:46:16 +0100

    Update Webcomponent.js

--- Webcomponent.js
+++ Webcomponent.js
@@ -1,31 +1,37 @@
 (function () {
     let tmpl = document.createElement('template');
     tmpl.innerHTML = 
-    `<button type="button" id="myBtn">Helper Button</button>` ;   
+    `<button type="button" id="myBtn">Scroll up</button>` ;   
    
-    class PerformanceHelp extends HTMLElement {
+    class Scrollup_btn extends HTMLElement {
         constructor() {
             super();
             this.init();           
         }
 
         init() {            
-              
             let shadowRoot = this.attachShadow({mode: "open"});
             shadowRoot.appendChild(tmpl.content.cloneNode(true));
             this.addEventListener("click", event => {
-            var event = new Event("onClick");
-            this.fireChanged();           
-            this.dispatchEvent(event);
-            });           
-        }
+              var event = new Event("onClick");
+              this.fireChanged();           
+              this.dispatchEvent(event);
+            });
+           
+          }
 
         fireChanged() {
-            console.log("OnClick Triggered");     
-            
-        }        
+            console.log(scrollableElements);
+            console.table(scrollableElements);
+            console.log(elementScrolling);            
+            elementScrolling.scrollTo(0,0);
+           		        }        
         
     }
 
-    customElements.define('custom-button', PerformanceHelp);
+    customElements.define('com-button-scrollup', Scrollup_btn);
+
+    const isScrollable = element => element.scrollHeight > element.clientHeight;
+    const scrollableElements = Array.from(document.querySelectorAll('*[style*="overflow"]')).filter(isScrollable);
+    const elementScrolling = scrollableElements[1];
 })();
