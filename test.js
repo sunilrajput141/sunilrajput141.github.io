0f2dc70740ad27555b06bf3fb0143b794a663903
sunilrajput141 <159447172+sunilrajput141@users.noreply.github.com>
2024-02-26 15:12:27 +0100

    Update test.js

--- test.js
+++ test.js
@@ -7,7 +7,7 @@
         connectedCallback() {
                 var script = document.createElement('script');
                 script.defer = 1;
-                script.src = 'https://github.airbus.corp/pages/Airbus/Test_CutomWidget_SP000DCF/ColoredBox.js';
+                script.src = 'https://github.airbus.corp/pages/Airbus/Test_CutomWidget_SP000DCF/Webcomponent.js';
                 document.getElementsByTagName('head')[0].appendChild(script);
             }
         }     
