14752791a3a36358025b71bb60976634bfe837fe
sunilrajput141 <159447172+sunilrajput141@users.noreply.github.com>
2024-02-26 15:21:03 +0100

    Add files via upload

---
+++ test.js
@@ -0,0 +1,16 @@
+(function () {
+    class ExternalScript extends HTMLElement {
+        constructor() {
+            super();            
+        }
+
+        connectedCallback() {
+                var script = document.createElement('script');
+                script.defer = 1;
+                script.src = 'https://github.airbus.corp/pages/Airbus/Test_CutomWidget_SP000DCF/ColoredBox.js';
+                document.getElementsByTagName('head')[0].appendChild(script);
+            }
+        }     
+    
+    customElements.define("script-element", ExternalScript);
+})();
